2f484aa532bb7a8033b0ee6497b1e200c541c071
sunandanbarman <sunandan.barman@gmail.com>
2016-04-01 01:18:00 -0400

    QDA working;95% accuracy

--- script.py
+++ script.py
@@ -13,19 +13,68 @@ import sys
 # The entry on column 0 are the mean values of X where trueLabel == 1
 # e.g. means[0][0] = mean of all entries in X where along column 0, and trueLabel == 1
 # means[1][0] == mean of all entries in X where along column 1, and trueLabel == 1
-def findMeanOnGivenAxisByClass(X,y,k):
+def findMeanByClass(X,y,k):
 	means = np.zeros([X.shape[1],k], dtype=float)
 	for i in range(1,k+1):
 		row_num_with_i = np.where(y==i)[0] #all the rows from y where the value == i
 		trainMatrix    = X[row_num_with_i,:] # all the rows from X where the true label of the entry == i
 		#print "trainMatrix :",trainMatrix
 		means[:,i-1]   = np.mean(trainMatrix,axis=0).transpose() #axis=0 means along vertical axis,
+
 		#print "means :",means[:,i-1]
 	return means
+
+#Same logic as findMeanByClass, we return an additional list of covariance matrix for each mean we find
+#Used in QDA analysis
+def findMeanByClassWithCovar(X,y,k):
+	means = np.zeros([X.shape[1],k], dtype=float)
+	covarList = []
+	for i in range(1,k+1):
+		row_num_with_i = np.where(y==i)[0] #all the rows from y where the value == i
+		trainMatrix    = X[row_num_with_i,:] # all the rows from X where the true label of the entry == i
+		#print "trainMatrix :",trainMatrix
+		means[:,i-1]   = np.mean(trainMatrix,axis=0).transpose() #axis=0 means along vertical axis,
+		covarList.append(np.cov(np.transpose(trainMatrix)))
+		#print "means :",means[:,i-1]
+	return means,covarList
 
 def findClassesInY(y):
 	k = np.unique(y)
 	return k,len(k)
+
+""""
+Generates the predicted label from the p(x) formula
+Refer eqn 3.1 in slide B.3 for formula
+"""
+def getPredictedLabel(testX_transpose,means,invCov,numClass,constantTerm):
+	prediction = 0
+	pdf = 0.0
+	res = 0.0
+	pdfList = []
+	for j in range(numClass): #for all columns in means matrix
+		row = testX_transpose - means[:,j]
+		res = constantTerm * np.exp((-1/2) * np.dot( np.dot( np.transpose(row) , invCov)  ,row))
+		pdfList.append(res)
+	prediction = pdfList.index(max(pdfList)) + 1
+	return prediction
+
+""""
+Generates the predicted label from the p(x) formula
+Refer eqn 3.1 in slide B.3 for formula
+"""
+def getPredictedLabelForQDA(testX_transpose,means,invCov,numClass,constantTerm):
+	prediction = 0
+	pdf = 0.0
+	res = 0.0
+	pdfList = []
+	for j in range(numClass): #for all columns in means matrix
+		row = testX_transpose - means[:,j]
+		res = constantTerm[j] * np.exp((-1/2) * np.dot( np.dot( np.transpose(row) , invCov[j])  ,row))
+		pdfList.append(res)
+	prediction = pdfList.index(max(pdfList)) + 1
+	return prediction
+
+"""%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
 #
 def ldaLearn(X,y):
 	#print X
@@ -46,7 +95,7 @@ def ldaLearn(X,y):
 	means = np.zeros([d,k], dtype=float) # 2*5 matrix
 	covmat= np.array([d,d], dtype=float) #2*2 matrix
 
-	means = findMeanOnGivenAxisByClass(X,y,k)
+	means = findMeanByClass(X,y,k)
 	covmat = np.cov(np.transpose(X))
 	return means,covmat
 
@@ -60,26 +109,23 @@ def qdaLearn(X,y):
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
 
     # IMPLEMENT THIS METHOD
-	#N =
-	means   = np.array([])
-	covmats = np.array([])
-	return means,covmats
+	"""
+	The implementation of this method is almost the same as ldaLearn
+	The process of finding mean is exactly like ldaLearn
+	covmats is now a list of covariance matrices
+	"""
+	#print X.shape ==> 150 *2
+	#print y.shape ==> 150 *1
+	N = X.shape[0] #number of elements
+	d = X.shape[1] #number of attributes
+	uniqueArray,k = findClassesInY(y); #number of classes
 
-""""
-Generates the predicted label from the p(x) formula
-Refer eqn 3.1 in slide B.3 for formula
-"""
-def getPredictedLabel(testX_transpose,means,invCov,numClass,constantTerm):
-	prediction = 0
-	pdf = 0.0
-	res = 0.0
-	pdfList = []
-	for j in range(numClass): #for all columns in means matrix
-		row = testX_transpose - means[:,j]
-		res = constantTerm * np.exp((-1/2) * np.dot( np.dot( np.transpose(row) , invCov)  ,(row) ))
-		pdfList.append(res)
-	prediction = pdfList.index(max(pdfList)) + 1
-	return prediction
+	means = np.zeros([d,k], dtype=float) # 2*5 matrix
+	#covmat= np.zeros([np.array([d,d], dtype=float)]) #2*2 matrix
+
+	means,covmats = findMeanByClassWithCovar(X,y,k)
+
+	return means,covmats
 
 def ldaTest(means,covmat,Xtest,ytest):
     # Inputs
@@ -101,11 +147,8 @@ def ldaTest(means,covmat,Xtest,ytest):
 
 	constantTerm = 1 / (sqrt(np.power(2 * pi,numClass))  * sqrt(det(covmat))) #the constant term in p(x) formula
 
-	"""The idea is to look at all the test examples given in Xtest, and apply the formula for computing p(x)
-	#to the dataset
-	The predicted label will be the one where the p(x) is maximum (different values of means will be tried out)
-	NOTE : We have ignored the term det(covar) * 1/ pow(root(2*pi),d)
-	 	   We can do this as this will be the same for all predictions, and hence does not affect our predictions"""
+	"""The idea is to look at all the test examples given in Xtest, and apply the formula for computing p(x) to the dataset
+	The predicted label will be the one where the p(x) is maximum (different values of means will be tried out)"""
 
 	for i in range(N): #for all training rows
 		pdf = 0
@@ -128,8 +171,37 @@ def qdaTest(means,covmats,Xtest,ytest):
     # ypred - N x 1 column vector indicating the predicted labels
 
     # IMPLEMENT THIS METHOD
-	acc =0
-	ypred = np.array([])
+
+	acc   =0.0;
+	N 	  = Xtest.shape[0] # number of test examples
+	numClass = means.shape[1] # number of means
+	ypred = np.zeros([N,1], dtype = float);
+	invCov= np.copy(covmats) #creates list of same size, basically that's all I use it for, all values are over-written later
+	#print "numClass ",numClass
+	print len(covmats)
+	for i in range(numClass): #for each of the k class, compute inverse(sigma)
+		invCov[i] = inv(covmats[i])
+
+	constantTerm = np.zeros(numClass) # list of length numClass
+	constantValue = sqrt(np.power(2 * pi,numClass))
+	for i in range(numClass):
+		constantTerm[i] = 1 / (constantValue  * sqrt(det(covmats[i]))) #the constant term in p(x) formula
+
+	"""
+	The idea is to look at all the test examples given in Xtest, and apply the formula for computing p(x) to the dataset
+	The predicted label will be the one where the p(x) is maximum (different values of means will be tried out)
+	"""
+
+	for i in range(N): #for all training rows
+		pdf = 0
+		testX_transpose = np.transpose(Xtest[i,:]) #required for (X-mu) operation
+		#for k in range(numClass):
+		prediction = getPredictedLabelForQDA(testX_transpose,means,invCov,numClass,constantTerm)
+		#print "prediction ",prediction
+		if (prediction == ytest[i]): #check if prediction matches
+			acc += 1 #correct prediction
+			ypred[i] = prediction
+	acc = acc / N #prediction accuracy percentage
 	return acc,ypred
 
 def learnOLERegression(X,y):

02c5adf04ed01bcd0e6f2055e7784bcdee7643a7
sunandanbarman <sunandan.barman@gmail.com>
2016-03-31 23:43:09 -0400

    LDA working

--- script.py
+++ script.py
@@ -8,20 +8,46 @@ import matplotlib.pyplot as plt
 import pickle
 import sys
 
+
+# The mean matrix is a 2 * 5 matrix
+# The entry on column 0 are the mean values of X where trueLabel == 1
+# e.g. means[0][0] = mean of all entries in X where along column 0, and trueLabel == 1
+# means[1][0] == mean of all entries in X where along column 1, and trueLabel == 1
+def findMeanOnGivenAxisByClass(X,y,k):
+	means = np.zeros([X.shape[1],k], dtype=float)
+	for i in range(1,k+1):
+		row_num_with_i = np.where(y==i)[0] #all the rows from y where the value == i
+		trainMatrix    = X[row_num_with_i,:] # all the rows from X where the true label of the entry == i
+		#print "trainMatrix :",trainMatrix
+		means[:,i-1]   = np.mean(trainMatrix,axis=0).transpose() #axis=0 means along vertical axis,
+		#print "means :",means[:,i-1]
+	return means
+
+def findClassesInY(y):
+	k = np.unique(y)
+	return k,len(k)
+#
 def ldaLearn(X,y):
-	
-	print X.shape
-	print y.shape
+	#print X
+	# print X.shape
+	# print y.shape
+	N = X.shape[0] #number of elements
+	d = X.shape[1] #number of attributes
+	uniqueArray,k = findClassesInY(y); #number of classes
     # Inputs
     # X - a N x d matrix with each row corresponding to a training example
     # y - a N x 1 column vector indicating the labels for each training example
     #
     # Outputs
     # means - A d x k matrix containing learnt means for each of the k classes
-    # covmat - A single d x d learnt covariance matrix 
-    
+    # covmat - A single d x d learnt covariance matrix
+
     # IMPLEMENT THIS METHOD
-    
+	means = np.zeros([d,k], dtype=float) # 2*5 matrix
+	covmat= np.array([d,d], dtype=float) #2*2 matrix
+
+	means = findMeanOnGivenAxisByClass(X,y,k)
+	covmat = np.cov(np.transpose(X))
 	return means,covmat
 
 def qdaLearn(X,y):
@@ -32,10 +58,28 @@ def qdaLearn(X,y):
     # Outputs
     # means - A d x k matrix containing learnt means for each of the k classes
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
-    
+
     # IMPLEMENT THIS METHOD
-    
-    return means,covmats
+	#N =
+	means   = np.array([])
+	covmats = np.array([])
+	return means,covmats
+
+""""
+Generates the predicted label from the p(x) formula
+Refer eqn 3.1 in slide B.3 for formula
+"""
+def getPredictedLabel(testX_transpose,means,invCov,numClass,constantTerm):
+	prediction = 0
+	pdf = 0.0
+	res = 0.0
+	pdfList = []
+	for j in range(numClass): #for all columns in means matrix
+		row = testX_transpose - means[:,j]
+		res = constantTerm * np.exp((-1/2) * np.dot( np.dot( np.transpose(row) , invCov)  ,(row) ))
+		pdfList.append(res)
+	prediction = pdfList.index(max(pdfList)) + 1
+	return prediction
 
 def ldaTest(means,covmat,Xtest,ytest):
     # Inputs
@@ -47,7 +91,32 @@ def ldaTest(means,covmat,Xtest,ytest):
     # ypred - N x 1 column vector indicating the predicted labels
 
     # IMPLEMENT THIS METHOD
-    return acc,ypred
+	#X = 100*2 matrix, Y = 100*1 matrix
+	#means = 2*5 , covmat = 2*2 matrix
+	acc   =0.0;
+	N 	  = Xtest.shape[0] # number of test examples
+	numClass = means.shape[1] # number of means
+	ypred = np.zeros([N,1], dtype = float);
+	invCov= inv(covmat) #compute inverse(sigma)
+
+	constantTerm = 1 / (sqrt(np.power(2 * pi,numClass))  * sqrt(det(covmat))) #the constant term in p(x) formula
+
+	"""The idea is to look at all the test examples given in Xtest, and apply the formula for computing p(x)
+	#to the dataset
+	The predicted label will be the one where the p(x) is maximum (different values of means will be tried out)
+	NOTE : We have ignored the term det(covar) * 1/ pow(root(2*pi),d)
+	 	   We can do this as this will be the same for all predictions, and hence does not affect our predictions"""
+
+	for i in range(N): #for all training rows
+		pdf = 0
+		testX_transpose = np.transpose(Xtest[i,:]) #required for (X-mu) operation
+		prediction = getPredictedLabel(testX_transpose,means,invCov,numClass,constantTerm)
+		#print "prediction ",prediction
+		if (prediction == ytest[i]): #check if prediction matches
+			acc += 1 #correct prediction
+			ypred[i] = prediction
+	acc = acc / N #prediction accuracy percentage
+	return acc,ypred
 
 def qdaTest(means,covmats,Xtest,ytest):
     # Inputs
@@ -59,27 +128,31 @@ def qdaTest(means,covmats,Xtest,ytest):
     # ypred - N x 1 column vector indicating the predicted labels
 
     # IMPLEMENT THIS METHOD
-    return acc,ypred
+	acc =0
+	ypred = np.array([])
+	return acc,ypred
 
 def learnOLERegression(X,y):
-    # Inputs:                                                         
-    # X = N x d 
-    # y = N x 1                                                               
-    # Output: 
-    # w = d x 1                                                                
-    # IMPLEMENT THIS METHOD                                                   
-    return w
+    # Inputs:
+    # X = N x d
+    # y = N x 1
+    # Output:
+    # w = d x 1
+    # IMPLEMENT THIS METHOD
+	w = np.array([])
+	return w
 
 def learnRidgeRegression(X,y,lambd):
     # Inputs:
-    # X = N x d                                                               
-    # y = N x 1 
+    # X = N x d
+    # y = N x 1
     # lambd = ridge parameter (scalar)
-    # Output:                                                                  
-    # w = d x 1                                                                
+    # Output:
+    # w = d x 1
 
-    # IMPLEMENT THIS METHOD                                                   
-    return w
+    # IMPLEMENT THIS METHOD
+	w = np.array([])
+	return w
 
 def testOLERegression(w,Xtest,ytest):
     # Inputs:
@@ -88,32 +161,34 @@ def testOLERegression(w,Xtest,ytest):
     # ytest = X x 1
     # Output:
     # rmse
-    
+
     # IMPLEMENT THIS METHOD
-    return rmse
+	rmse = np.array([])
+	return rmse
 
 def regressionObjVal(w, X, y, lambd):
 
     # compute squared error (scalar) and gradient of squared error with respect
     # to w (vector) for the given data X and y and the regularization parameter
-    # lambda                                                                  
+    # lambda
 
-    # IMPLEMENT THIS METHOD                                             
+    # IMPLEMENT THIS METHOD
+
     return error, error_grad
 
 def mapNonLinear(x,p):
-    # Inputs:                                                                  
-    # x - a single column vector (N x 1)                                       
-    # p - integer (>= 0)                                                       
-    # Outputs:                                                                 
-    # Xd - (N x (d+1))                                                         
+    # Inputs:
+    # x - a single column vector (N x 1)
+    # p - integer (>= 0)
+    # Outputs:
+    # Xd - (N x (d+1))
     # IMPLEMENT THIS METHOD
     return Xd
 
 # Main script
 
 # Problem 1
-# load the sample data                                                                 
+# load the sample data
 if sys.version_info.major == 2:
     X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'))
 else:
@@ -182,7 +257,7 @@ k = 101
 lambdas = np.linspace(0, 1, num=k)
 i = 0
 rmses4 = np.zeros((k,1))
-opts = {'maxiter' : 100}    # Preferred value.                                                
+opts = {'maxiter' : 100}    # Preferred value.
 w_init = np.ones((X_i.shape[1],1))
 for lambd in lambdas:
     args = (X_i, y, lambd)
